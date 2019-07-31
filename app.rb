4af05d6e28046f0dafe46daba0fb2bb461f0e18f
Jamie Vullo <jvullo@protonmail.com>
2019-07-31 16:37:07 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -1,4 +1,15 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  
+  get '/' do
+    "My name is Jamie"
+  end
+  
+  get '/' do
+    "My hometown is Taylor, PA"
+  end
+  
+  get '/' do
+    "My favorite song is ?"
 end
